0f363475adcad06fa583a321a9b3c5cc40d83fbe
Aleksey Samoylov <alekseysamoylov89@gmail.com>
2018-09-09 12:06:33 +0300

    task old type fix

--- src/main/kotlin/com/alekseysamoylov/nastushenka/config/SecurityConfig.kt
+++ src/main/kotlin/com/alekseysamoylov/nastushenka/config/SecurityConfig.kt
@@ -1,11 +1,29 @@
 package com.alekseysamoylov.nastushenka.config
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.web.firewall.HttpFirewall
+import org.springframework.security.web.firewall.StrictHttpFirewall
+
 
 @Configuration
 class SecurityConfig : WebSecurityConfigurerAdapter() {
+
+  @Bean
+  fun allowUrlEncodedSlashHttpFirewall(): HttpFirewall {
+    val firewall = StrictHttpFirewall()
+    firewall.setAllowUrlEncodedSlash(true)
+    return firewall
+  }
+
+  override fun configure(web: WebSecurity) {
+    super.configure(web);
+    web.httpFirewall(allowUrlEncodedSlashHttpFirewall());
+  }
+
   override fun configure(http: HttpSecurity) {
     // @formatter:off
     http
